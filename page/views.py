55ce3dd6436930bad8ef10c1c0e5f787d514a9d6
donghyun <gimdh1218@naver.com>
2020-11-17 21:34:50 +0900

    공지사항 모델 생성

--- page/views.py
+++ page/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Notice
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def notice(request):
+    notices = Notice.objects.all()
+    return render(request, 'notice.html', {'notices': notices})
