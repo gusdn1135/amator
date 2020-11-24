60a34674fbc6ff9f6336826e242a8cac0e5a7f50
donghyun <gimdh1218@naver.com>
2020-11-24 22:11:28 +0900

    mysql 적용하는 중

--- amator/settings.py
+++ amator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +75,8 @@ WSGI_APPLICATION = 'amator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
---
+++ my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    [1]
+        'NAME': 'amator',                        [2]
+        'USER': 'root',                          [3]
+        'PASSWORD': 'kdh018803',                 [4]
+        'HOST': 'localhost',                     [5]
+        'PORT': '3306',                          [6]
+    }
+}
